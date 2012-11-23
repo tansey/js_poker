8beb657f80752478818a00f42efcc03da565b10a
Wesley Tansey <wes.tansey@gmail.com>
2012-11-23 01:26:14 -0500

    initial commit. most graphics and css finished.

---
+++ static/js/poker.js
@@ -0,0 +1,52 @@
+var game;
+var server;
+
+$(function(){
+    // Connect to the server
+
+    // Load game
+})
+
+function catchUp(history){
+    
+}
+
+function setHoleCards(hc, seat){
+    
+}
+
+function setBoardCards(board) {
+    
+}
+
+function setBet(bet, seat){
+
+}
+
+function getCardSrc(card) {
+    
+}
+
+function getChipSrc(chipValue){
+    
+}
+
+function foldClicked(){
+    
+}
+
+function callClicked(){
+    
+}
+
+function raiseClicked(){
+    
+}
+
+function resetTable(){
+    
+}
+
+function moveButton(seat){
+    
+}
